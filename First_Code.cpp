54107e1a5a3a44b539bd07be68a0c9f4eb594a69
KARTIK PANDEY <kartik0pandey00@gmail.com>
2025-10-14 13:37:23 +0530

    Add files via upload

---
+++ First_Code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout<<"Hello World"<<endl;
+    cout<<"It's Kartik\n";
+    int num = 6;
+    cout<<num<<endl; 
+    cout<< num * 3<< endl ;
+    cout<< "num is 06\n";
+    cout<< "Thank You";
+}
