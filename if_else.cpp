b2a4f546b4d26d7e1e3ac024324bc5125872e59c
KARTIK PANDEY <kartik0pandey00@gmail.com>
2025-10-15 20:36:12 +0530

    Add files via upload

---
+++ if_else.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int package;
+    cout<<"enter your package:";
+    cin>> package;
+
+    if(package>10)
+    {
+        cout<<"offer is accepted";
+    }
+    else
+    {
+        cout<<"offer is rejected";
+    }
+}
