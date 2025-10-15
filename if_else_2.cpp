f8312247023853910f1ce500742414da6e5722d1
KARTIK PANDEY <kartik0pandey00@gmail.com>
2025-10-15 20:36:36 +0530

    Add files via upload

---
+++ if_else_2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int marks;
+    cout<<"enter your marks: ";
+    cin>> marks;
+
+    if (marks>=33)
+    {
+        cout<<"You are Pass";
+    }
+    else
+    {
+        cout<<"you are fail";
+    }
+    
+}
