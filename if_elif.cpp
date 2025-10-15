06bcbe0f4f9042f77d1d5065591669e6cbff2322
KARTIK PANDEY <kartik0pandey00@gmail.com>
2025-10-15 20:35:23 +0530

    Add files via upload

---
+++ if_elif.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char character;
+    for (int i =0; i<=5; i++)
+    {
+        cout<<"Enter Alphabet: ";
+    cin>> character;
+    
+    if (character=='a')
+    {
+        cout<<"This is Vowel"<<endl;
+    }
+    else if(character == 'e')
+    {
+        cout<<"This is Vowel"<<endl;
+    }
+    else if(character == 'i')
+    {
+        cout<<"This is Vowel"<<endl;
+    }
+    else if(character == 'o')
+    {
+        cout<<"This is Vowel"<<endl;
+    }
+    else if(character == 'u')
+    {
+        cout<<"This is Vowel"<<endl;
+    }
+    else
+    {
+        cout<<"This is Consonant"<<endl;
+    }
+
+    }
+    
+}
