510113b3567df987aa90fc7119600ebda2d20e71
KARTIK PANDEY <kartik0pandey00@gmail.com>
2025-10-18 18:09:28 +0530

    Add files via upload

---
+++ type_casting_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int a = 97;
+    char b = 'K';
+     
+    // b=a;
+    // cout<< b <<endl; 
+    a=b;
+    cout<< a<<endl;
+
+    b=a;
+    cout<< b<<endl;
+
+}
