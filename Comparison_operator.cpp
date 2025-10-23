30dcc18ae54b861dbe72f2ac388937bd17e28b96
KARTIK PANDEY <kartik0pandey00@gmail.com>
2025-10-23 23:48:01 +0530

    Add files via upload

---
+++ Comparison_operator.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    int b;
+    cout<<"Enter A:- ";
+    cin>>a;
+    cout<<"Enter B:- ";
+    cin>>b;
+    
+    if(a>b)
+    {
+        cout<<"yes"<<endl;
+    }
+    else if(a==b)
+    {
+        cout<<"Both Number is Equal";
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+    
+
+}
