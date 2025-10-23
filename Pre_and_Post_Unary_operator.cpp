23b15fa68c739ee5277077a821d7f348a5db0156
KARTIK PANDEY <kartik0pandey00@gmail.com>
2025-10-23 23:46:11 +0530

    Add files via upload

---
+++ Pre_and_Post_Unary_operator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=10;
+    cout<<a++<<endl;
+    cout<<a<<endl;
+    int b=a;
+    cout<<b<<endl;
+    cout<<--a<<endl;
+    cout<<a--<<endl;
+    cout<<a<<endl;
+    
+
+}
---
+++ Unary_Arithmetic_operator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a, b;
+
+    cout<<"Enter the Number:- ";
+    cin>>a;
+
+    b=a;
+    cout<<b<<endl;
+    cout<<a++<<endl;
+    cout<<a<<endl;
+    b=a;
+    cout<<b<<endl;
+}
