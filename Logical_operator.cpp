1e756e7b58d5d457bb35f05c14bb3930db93b53e
KARTIK PANDEY <kartik0pandey00@gmail.com>
2025-10-23 23:48:24 +0530

    Add files via upload

---
+++ Logical_operator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a=2;
+    int b=3;
+    int C= a&&b;
+    int d=3&&5;
+    cout<<C<<endl;
+    cout<<d<<endl;
+    cout<<0||2;
+    cout<<endl<<3||2;
+
+
+
+
+}
