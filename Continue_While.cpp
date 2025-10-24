fe19a428f77f702cffa3d6cdbc33273a35dde54c
KARTIK PANDEY <kartik0pandey00@gmail.com>
2025-10-25 01:52:44 +0530

    Add files via upload

---
+++ Continue_While.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i=1;
+    while (i<=10)
+    {
+        /* code */
+        if(i==6)
+        continue;
+        cout<<i<<endl;
+        i=i+1;
+    }
+    
+}
