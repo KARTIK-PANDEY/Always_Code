8299fe614f3c6454c16d77ea6426b644019a8b6b
KARTIK PANDEY <kartik0pandey00@gmail.com>
2025-10-24 20:18:36 +0530

    Add files via upload

---
+++ Bitwise_operator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a=2;
+    int b=3;
+    int c=a&b;
+    cout<<c<<endl;
+    int d=a^b;
+    cout<<d;
+    int k=a|b;
+    cout<<endl<<k;
+    int p=~5;
+    int y=~-9;
+    cout<<endl<<p<<endl<<y;
+
+}
