84113b163772f28074b0871e5d103bf56cd4f46f
KARTIK PANDEY <kartik0pandey00@gmail.com>
2025-10-25 01:53:56 +0530

    Add files via upload

---
+++ Switch_case.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the Number:- ";
+    cin>>n;
+
+    switch (n)
+    {
+        case 1:
+        cout<<"KARTIK";
+        break;
+
+        case 2:
+        cout<<"PANDEY";
+        break;
+
+        case 3:
+        cout<<"PANDEY JIII";
+        break;
+
+        case 4:
+        cout<<"BILLIONAIRE";
+        break;
+
+        default:
+        cout<<"Call Pandey jii";
+    }
+}
