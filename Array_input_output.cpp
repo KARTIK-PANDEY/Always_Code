45e92d5245107e737365e6162b584246f4518310
KARTIK PANDEY <kartik0pandey00@gmail.com>
2025-10-31 00:05:56 +0530

    Add files via upload

---
+++ Array_input_output.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {4, 5, 6, 3, 8};
+
+    for(int i=0; i<5; i++)
+    {
+        cout<<"The "<< i <<" "<< "value of Array is:- "<<arr[i]<<endl;
+    }
+
+
+}
